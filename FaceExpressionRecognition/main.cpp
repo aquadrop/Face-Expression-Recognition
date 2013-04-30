b1a388a6ccb44ce3a06d3fa028c0eaa9781a4541
Jianwei Xu <jianwei.xu@mail.utoronto.ca>
2013-04-30 16:59:57 -0400

    Initial Commit

---
+++ FaceExpressionRecognition/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FaceExpressionRecognition
+//
+//  Created by Jianwei Xu on 30/04/2013.
+//  Copyright (c) 2013 Jianwei Xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
